cbe46716b5a3cc2ef7c5a46fb4cbb315b26f0354
CHOW Chun Ho <chunhochow@users.noreply.github.com>
2024-08-22 14:26:02 -0700

    2022/ pipeline_config use filename with .csv directly, 00-preprocess: clean up unneeded creation of columns

--- 2022/00-preprocess.py
+++ 2022/00-preprocess.py
@@ -1,8 +1,7 @@
 """
-minor changes to CSV files:
-person: add person_id_sfcta column by concatenating hh_id and person_num
-trip: add columns: depart_time, arrive_time, person_id_sfcta, trip_id_sfcta
-location: add person_id, person_id_sfcta, trip_id, trip_id_sfcta from trip table
+copy all CSV files from raw_dir to 00-preprocess dir with minor changes:
+trip: add columns: depart_time, arrive_time
+location: add person_id from trip table
 """
 
 import argparse
@@ -14,47 +13,29 @@ import pandas as pd
 
 
 def preprocess(config):
-    raw_dir = Path(config["raw_dir"])
+    raw_dir = Path(config["raw"]["dir"])
     preprocess_dir = Path(config["00-preprocess"]["dir"])
     preprocess_dir.mkdir(exist_ok=True)
 
-    # TODO move the construction of filenames into a utils.py
-    hh_filename = f"{config["hh_filename_stem"]}.csv"
-    person_filename = f"{config["person_filename_stem"]}.csv"
-    day_filename = f"{config["day_filename_stem"]}.csv"
-    trip_filename = f"{config["trip_filename_stem"]}.csv"
-    location_filename = f"{config["location_filename_stem"]}.csv"
-    vehicle_filename = f"{config["vehicle_filename_stem"]}.csv"
-
-    _ = preprocess_person(raw_dir, preprocess_dir, person_filename)
-    trip = preprocess_trip(raw_dir, preprocess_dir, trip_filename)
-    _ = preprocess_location(raw_dir, preprocess_dir, location_filename, trip)
+    trip = preprocess_trip(raw_dir, preprocess_dir, config["trip_filename"])
+    _ = preprocess_location(raw_dir, preprocess_dir, config["location_filename"], trip)
 
     # copying the unchanged files too for ease of use / easy backward compatibility;
-    # not sure if we should copy files that are not changed
-    copy2(raw_dir / hh_filename, preprocess_dir / hh_filename)
+    # not sure for logical clarity if we should copy files that are not changed
+    day_filename = config["day_filename"]
+    hh_filename = config["hh_filename"]
+    person_filename = config["person_filename"]
+    vehicle_filename = config["vehicle_filename"]
     copy2(raw_dir / day_filename, preprocess_dir / day_filename)
+    copy2(raw_dir / hh_filename, preprocess_dir / hh_filename)
+    copy2(raw_dir / person_filename, preprocess_dir / person_filename)
     copy2(raw_dir / vehicle_filename, preprocess_dir / vehicle_filename)
     return
 
 
-def preprocess_person(raw_dir, preprocess_dir, person_filename):
-    person = pd.read_csv(raw_dir / person_filename)
-    print("persons:", len(person))
-    # TODO TO VERIFY: as of 2022 data, seems like no longer needed to create
-    # person_id_sfcta because person_id is exactly what this new column is
-    person["person_id_sfcta"] = person.apply(
-        lambda x: "{:d}{:02d}".format(x["hh_id"], x["person_num"]), axis=1
-    ).astype("int64")
-    print("persons:", len(person))
-    person.to_csv(preprocess_dir / person_filename, index=False)
-    return person
-
-
 def preprocess_trip(raw_dir, preprocess_dir, trip_filename):
     trip = pd.read_csv(raw_dir / trip_filename)
-    print("trips:", len(trip))
-
+    print("trip raw len:", len(trip))
     if "depart_seconds" in trip.columns:
         trip.rename(columns={"depart_seconds": "depart_second"}, inplace=True)
     trip["depart_time"] = trip.apply(
@@ -69,29 +50,21 @@ def preprocess_trip(raw_dir, preprocess_dir, trip_filename):
         ),
         axis=1,
     )
-
-    trip["person_id_sfcta"] = trip.apply(
-        lambda x: "{:d}{:02d}".format(x["hh_id"], x["person_num"]), axis=1
-    ).astype("int64")
-    trip["trip_id_sfcta"] = trip.apply(
-        lambda x: "{:d}{:03d}".format(x["person_id_sfcta"], x["trip_num"]), axis=1
-    ).astype("int64")
-
-    print("trips:", len(trip))
+    print("trip preprocessed len:", len(trip))
     trip.to_csv(preprocess_dir / trip_filename, index=False)
     return trip
 
 
 def preprocess_location(raw_dir, preprocess_dir, location_filename, trip):
     location = pd.read_csv(raw_dir / location_filename)
-    print("locations:", len(location))
+    print("location raw len:", len(location))
     location = pd.merge(
-        trip[["person_id", "person_id_sfcta", "trip_id", "trip_id_sfcta"]],
         location,
+        trip[["trip_id", "person_id"]],
         on="trip_id",
-        how="right",
+        how="left",
     )
-    print("locations:", len(location))
+    print("location preprocessed len:", len(location))
     location.to_csv(preprocess_dir / location_filename, index=False)
     return location
 
