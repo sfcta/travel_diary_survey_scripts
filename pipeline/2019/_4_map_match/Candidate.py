159b041ac2309f176781124a123975fe6b3b6a64
CHOW Chun Ho <chunhochow@users.noreply.github.com>
2025-03-06 13:53:01 -0800

    pipeline/2019/: rm _4_map_match/ and SCAG/SANDAG py scripts

--- pipeline/2019/_4_map_match/Candidate.py
+++
@@ -1,8 +0,0 @@
-import map_matching as mm
-
-# Subclass the native Candidate class to support more attributes
-class Candidate(mm.Candidate):
-    def __init__(self, measurement, edge, location, distance):
-        super(Candidate, self).__init__(measurement=measurement, edge=edge, location=location, distance=distance)
-        self.lon = None
-        self.lat = None
